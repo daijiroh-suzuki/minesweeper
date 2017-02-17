25c9b4aa63f490ea2a826ef70a5feb3c03ed637c
'daijiroh' <'daijiroh.suzuki@gmail.com'>
2017-02-17 10:43:33 +0900

    コメント追加

--- Minesweeper/src/jp/dsuzuki/minesweeper/MainPanel.java
+++ Minesweeper/src/jp/dsuzuki/minesweeper/MainPanel.java
@@ -16,29 +16,29 @@ import jp.dsuzuki.minesweeper.main.parts.boad.MainBoad;
 public class MainPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	/** 盤面 */
 	private MainBoad boad;
-	
+
 	/**
 	 * コンストラクタ
 	 */
 	public MainPanel(int index) {
-		
+
 		Map<String, Integer> settingMap = CommonConstant.SETTING_LIST.get(index);
 		// パネルの幅を取得
 		int width = settingMap.get(CommonConstant.TILE_X) * settingMap.get(CommonConstant.TILE_SIZE) + 20;
 		// パネルの高さを取得
 		int height = settingMap.get(CommonConstant.TILE_Y) * settingMap.get(CommonConstant.TILE_SIZE) + 60;
-		
+
 		// パネルの推奨サイズを設定、pack()するときに必要
 		setPreferredSize(new Dimension(width, height));
-		
+
 		// ボタンを生成
 		JButton button = new JButton(CommonConstant.BUTTON_INIT);
 		// 盤面クラスを生成
 		boad = new MainBoad(button, index);
-		
+
 		// ボタンのアクションリスナーを設定
 		button.addMouseListener(
 				new MouseAdapter() {
@@ -49,12 +49,15 @@ public class MainPanel extends JPanel {
 						boad.init();
 					}
 				});
-		
+
 		add(button, BorderLayout.NORTH);
-		add(boad, BorderLayout.CENTER);		
+		add(boad, BorderLayout.CENTER);
 	}
-	
+
+	/**
+	 * 描画処理
+	 */
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);		
-	}	
+		super.paintComponent(g);
+	}
 }
