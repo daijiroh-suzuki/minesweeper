b383e71acb457741ae1501ea0251dee063f48f04
'daijiroh' <'daijiroh.suzuki@gmail.com'>
2018-02-07 17:21:31 +0900

    メニューから難易度を設定できるように変更

--- Minesweeper/src/jp/dsuzuki/minesweeper/Minesweeper.java
+++ Minesweeper/src/jp/dsuzuki/minesweeper/Minesweeper.java
@@ -1,11 +1,16 @@
 package jp.dsuzuki.minesweeper;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 
 import jp.dsuzuki.minesweeper.common.CommonConstant;
 import jp.dsuzuki.minesweeper.common.Difficulty;
@@ -15,7 +20,7 @@ import jp.dsuzuki.minesweeper.common.Difficulty;
  * @author daijiroh
  *
  */
-public class Minesweeper extends JFrame {
+public class Minesweeper extends JFrame implements ActionListener {
 
     private static final long serialVersionUID = 1L;
 
@@ -31,31 +36,94 @@ public class Minesweeper extends JFrame {
         JMenuBar menubar = new JMenuBar();
 
         // メニューを生成
-        JMenu menu1 = new JMenu("ファイル");
-        JMenu menu2 = new JMenu("編集");
+        JMenu mnFile = new JMenu("ファイル");
+        JMenu mnHelp = new JMenu("ヘルプ");
 
         // メニューアイテムを生成
-        JMenuItem menuitem1 = new JMenuItem("新規");
-        JMenuItem menuitem2 = new JMenuItem("終了");
+        JMenuItem mniNew     = new JMenuItem("新規");
+        JMenuItem mniExit    = new JMenuItem("終了");
+        JMenuItem mniVersion = new JMenuItem("バージョン情報");
+
+        JRadioButtonMenuItem mniDifficulty1 = new JRadioButtonMenuItem("初級");
+        JRadioButtonMenuItem mniDifficulty2 = new JRadioButtonMenuItem("中級");
+        JRadioButtonMenuItem mniDifficulty3 = new JRadioButtonMenuItem("上級");
+        mniDifficulty1.setSelected(true);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(mniDifficulty1);
+        group.add(mniDifficulty2);
+        group.add(mniDifficulty3);
+
+        mniNew.addActionListener(this);
+        mniExit.addActionListener(this);
+
+        mniDifficulty1.addActionListener(this);
+        mniDifficulty2.addActionListener(this);
+        mniDifficulty3.addActionListener(this);
 
         // メニューにメニューアイテムを追加
-        menu1.add(menuitem1);
-        menu1.add(menuitem2);
+        mnFile.add(mniNew);
+        mnFile.addSeparator();
+        mnFile.add(mniDifficulty1);
+        mnFile.add(mniDifficulty2);
+        mnFile.add(mniDifficulty3);
+        mnFile.addSeparator();
+        mnFile.add(mniExit);
+        mnHelp.add(mniVersion);
 
         // メニューバーにメニューを追加
-        menubar.add(menu1);
-        menubar.add(menu2);
+        menubar.add(mnFile);
+        menubar.add(mnHelp);
 
         // メニューバーをフレームに追加
         setJMenuBar(menubar);
 
-        // メインパネルを生成してフレームに追加
-        MainPanel panel = new MainPanel(Difficulty.BEGINNER);
+        // メインパネルをフレームに追加
+        setMainPanel(Difficulty.BEGINNER);
+    }
+
+    /**
+     * メインパネルをフレームに追加
+     *
+     * @param difficulty 難易度
+     */
+    private void setMainPanel(Difficulty difficulty) {
+
+        // メインパネルを生成
+        MainPanel panel = new MainPanel(difficulty);
+
+        // コンテントペインを取得
         Container contentPane = getContentPane();
-        contentPane.add(panel);
+        // コンテントペイン上のコンポーネントを削除
+        contentPane.removeAll();
+        // コンテントペインにメインパネルを追加
+        contentPane.add(panel, BorderLayout.CENTER);
 
         // パネルサイズに合わせてフレームサイズを自動設定
         pack();
+    }
+
+    /**
+     * メニューイベント処理
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String command = e.getActionCommand();
+
+        if("新規".equals(command)) {
+
+        } else if("初級".equals(command)) {
+            setMainPanel(Difficulty.BEGINNER);
+        } else if("中級".equals(command)) {
+            setMainPanel(Difficulty.MIDDLE);
+        } else if("上級".equals(command)) {
+            setMainPanel(Difficulty.ADVANCED);
+        } else if("終了".equals(command)) {
+            System.exit(0);
+        }
     }
 
     /**
